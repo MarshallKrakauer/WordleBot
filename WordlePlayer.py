4572e6e3a052e2c2047e541bfc9fa67d54960b77
Marshal Krakauer <marshallkrakauer@gmail.com>
2022-07-30 19:07:56 -0700

    check guesses

--- WordlePlayer.py
+++ WordlePlayer.py
@@ -8,6 +8,8 @@ import copy
 import collections
 
 original_df = pd.DataFrame(data={'word': FULL_LIST})
+LOSER_LIST = ['crack', 'foyer', 'fully', 'hatch', 'hitch', 'hound', 'hunch', 'kitty', 'liner', 'rarer', 'riser',
+              'river', 'roger', 'rover', 'rower', 'shame', 'taste', 'taunt', 'willy', 'wound']
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
 
@@ -16,6 +18,14 @@ def list_to_string(start_list):
     for x in start_list:
         result_str += x
     return result_str
+
+
+def count_g(last_guess_result):
+    g_counter = 0
+    for item in last_guess_result:
+        if item == 'G':
+            g_counter += 1
+    return g_counter
 
 
 def play_wordle(df, solution, guess_num, guess, noisy=True):
@@ -38,10 +48,11 @@ def play_wordle(df, solution, guess_num, guess, noisy=True):
         else:
             result_li[i] = 'X'
 
-    df, _ = filter_df(guess, list_to_string(result_li), df)
+    df, orig = filter_df(guess, list_to_string(result_li), df)
 
     df.sort_values('value', ascending=False, inplace=True)
     df.reset_index(inplace=True, drop=True)
+
     next_guess = df.loc[0, 'word']
 
     if noisy:
@@ -50,7 +61,8 @@ def play_wordle(df, solution, guess_num, guess, noisy=True):
               '\nguess number:', guess_num,
               '\nguess:', guess,
               '\nresult:', result_li,
-              '\nnext_guess:', next_guess, )
+              '\nnext_guess:', next_guess,
+              '\nremaining solutions:', len(df))
 
     if result_li != ['G', 'G', 'G', 'G', 'G']:
         return play_wordle(copy.deepcopy(df), solution, guess_num + 1, next_guess, noisy)
@@ -62,10 +74,10 @@ def play_wordle(df, solution, guess_num, guess, noisy=True):
 
 if __name__ == '__main__':
     sample_solution = random.choice(FULL_LIST)
-    guess_num_li = []
-    loser_li = []
+    guess_num_li, loser_li = [], []
 
-    for idx, word in enumerate(FULL_LIST):
+    # Run through the whole list, see how many of them get it right
+    for idx, word in enumerate(LOSER_LIST):
         if idx % 200 == 0 and idx > 0:
             print('current word:', word)
         num_guesses = play_wordle(copy.deepcopy(original_df), word, 1, 'crane', False)
@@ -73,14 +85,8 @@ if __name__ == '__main__':
             loser_li.append(word)
         guess_num_li.append(num_guesses)
 
+    # Show guess result and missed words
     guess_summary = collections.Counter(guess_num_li)
     guess_summary = sorted(guess_summary.items(), key=lambda i: i[0])
     print(guess_summary)
     print('losers:', loser_li)
-
-    '''df = copy.deepcopy(original_df)
-
-    df, orig = filter_df('crane', 'GXXYX', df)
-    df, orig = filter_df('conic', 'GXGYY', df)
-    print(len(df))
-    print(df.sort_values('value', ascending=False).head(10))'''
